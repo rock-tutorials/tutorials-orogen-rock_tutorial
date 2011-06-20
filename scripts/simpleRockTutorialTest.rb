f41160ad8119ac6fce7d08c7ff73d15bd0a922e1
Sascha Arnold <sascha.arnold@dfki.de>
2011-06-20 15:00:23 +0200

    added comments to the ruby scripts

--- scripts/simpleRockTutorialTest.rb
+++ scripts/simpleRockTutorialTest.rb
@@ -2,25 +2,31 @@ require 'orocos'
 require 'readline'
 include Orocos
 
+## Initialize orocos ##
 Orocos.initialize
 
+## Execute the deployment 'rock_tutorial' ##
 Orocos.run 'rock_tutorial' do
 
+## Get a specific task context ##
 rockControl = TaskContext.get 'rock_tutorial_control'
 rockVisualization = TaskContext.get 'rock_tutorial_visualization'
 
+## Connect the ports ##
 rockControl.pose.connect_to rockVisualization.pose
 
+## Create a sample writer for a port ##
 sampleWriter = rockControl.motion_command.writer
 
+## Start the tasks ##
 rockControl.start
 rockVisualization.start
 
+## Write motion command sample ##
 sample = sampleWriter.new_sample
 sample.translation = 1
 sample.rotation = 0.5
 sampleWriter.write(sample)
 
 Readline::readline('Press enter to exit')
-
 end
--- scripts/startRockTutorial.rb
+++ scripts/startRockTutorial.rb
@@ -2,23 +2,31 @@ require 'orocos'
 require 'readline'
 include Orocos
 
+## Initialize orocos ##
 Orocos.initialize
 
+## Execute the deployments 'rock_tutorial' and 'joystick_local' ##
 Orocos.run 'rock_tutorial', 'joystick_local' do
 
+## Get the specific task context ##
 rockControl = TaskContext.get 'rock_tutorial_control'
 rockVisualization = TaskContext.get 'rock_tutorial_visualization'
 joystickLocal = TaskContext.get 'joystick_local'
 
+## Connect the ports ##
 joystickLocal.motion_command.connect_to rockControl.motion_command
 rockControl.pose.connect_to rockVisualization.pose
 
-joystickLocal.configure
-joystickLocal.start
+## Set some properties ##
+joystickLocal.joystick_device = "/dev/input/js0"
 
+## Configure the tasks ##
+joystickLocal.configure
+
+## Start the tasks ##
+joystickLocal.start
 rockControl.start
 rockVisualization.start
 
 Readline::readline('Press enter to exit')
-
 end
