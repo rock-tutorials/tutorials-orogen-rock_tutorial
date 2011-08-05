74fc742fca2691c668026bff1fca3ea3c6bdf720
Janosch Machowinski <janosch.machowinski@dfki.de>
2011-08-05 17:44:07 +0200

    reworked scripts from tutorials

--- scripts/rockTutorial2.rb
+++ scripts/rockTutorial2.rb
@@ -1,13 +1,9 @@
 require 'orocos'
-require 'readline'
 require 'vizkit'
 include Orocos
 
 ## Initialize orocos ##
 Orocos.initialize
-
-##TODO THIS IS A BUG
-Orocos.load_typekit('base')
 
 ## create a widget for 3d display
 view3d = Vizkit.default_loader.create_widget('vizkit::QVizkitWidget')
@@ -18,30 +14,32 @@ view3d.show()
 ## load and add the 3d plugin for the rock
 vizkit_rock = view3d.createPlugin('rock_tutorial', 'RockVisualization')
 
-## Execute the deployment 'rock_tutorial' ##
-Orocos.run 'rock_tutorial' do
-
-    ## Get a specific task context ##
-    rockControl = TaskContext.get 'rock_tutorial_control'
-    
+## Execute the deployments 'rock_tutorial' and 'joystick_local' ##
+Orocos.run 'rock_tutorial', 'joystick' do
+  
     ## Connect port to vizkit plugin
-    con = Vizkit.connect_port_to 'rock_tutorial_control', 'pose', :auto_reconnect => true, :pull => false, :update_frequency => 33 do |sample, name|
+    con = Vizkit.connect_port_to 'rock_tutorial_control', 'pose', :update_frequency => 33 do |sample, name|
 	##pass every pose sample to our visualizer plugin
         vizkit_rock.updatePose(sample)
         sample
     end 
 
-    ## Create a sample writer for a port ##
-    sampleWriter = rockControl.motion_command.writer
-    
+    ## Get the specific task context ##
+    rockControl = TaskContext.get 'rock_tutorial_control'
+    joystick = TaskContext.get 'joystick'
+
+    ## Connect the ports ##
+    joystick.motion_command.connect_to rockControl.motion_command
+
+    ## Set some properties ##
+    joystick.device = "/dev/input/js0"
+
+    ## Configure the tasks ##
+    joystick.configure
+
     ## Start the tasks ##
+    joystick.start
     rockControl.start
-    
-    ## Write motion command sample ##
-    sample = sampleWriter.new_sample
-    sample.translation = 1
-    sample.rotation = 0.5
-    sampleWriter.write(sample)
 
     Vizkit.exec
 end
