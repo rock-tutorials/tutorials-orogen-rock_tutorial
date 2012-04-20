90a2dd6b477722876f4c37f027a51c10a912696d
Alexander Duda <alexander.duda@dfki.de>
2012-04-20 19:25:54 +0200

    cleanup rockTutorial3.rb

--- scripts/rockTutorial3.rb
+++ scripts/rockTutorial3.rb
@@ -1,41 +1,36 @@
-require 'orocos'
 require 'vizkit'
 include Orocos
 
 ## Initialize orocos ##
 Orocos.initialize
 
-## create a widget for 3d display
-view3d = Vizkit.default_loader.create_widget('vizkit::Vizkit3DWidget')
-
-#show it
-view3d.show()
+## create and show a widget for 3d display
+Vizkit.vizkit3d_widget.show
 
 ## load and add the 3d plugin for the rock
-vizkit_rock = view3d.createPlugin('rock_tutorial', 'RockVisualization')
+vizkit_rock = Vizkit.default_loader.RockVisualization
 
 ## create a widget that emulates a joystick
-joystickGui = Vizkit.default_loader.create_widget('VirtualJoystick')
+joystickGui = Vizkit.default_loader.VirtualJoystick
 
 #show it
 joystickGui.show()
 
+#Vizkit::ReaderWriterProxy.default_policy = {:port_proxy => nil, :init => true}
+
 ## Execute the deployments 'rock_tutorial' ##
 Orocos.run 'rock_tutorial::RockTutorialControl' => 'rock_tutorial_control' do
-  
-    ## Connect port to vizkit plugin
-    con = Vizkit.connect_port_to 'rock_tutorial_control', 'pose_samples', :update_frequency => 33 do |sample, name|
-	##pass every pose sample to our visualizer plugin
-        vizkit_rock.updateData(sample)
-        sample
-    end 
-
     ## Get the specific task context ##
     rockControl = TaskContext.get 'rock_tutorial_control'
+  
+    ## Connect port to vizkit plugin
+    rockControl.pose_samples.connect_to :update_frequency => 33 do |sample, name|
+	##pass every pose sample to our visualizer plugin
+        vizkit_rock.updateData(sample)
+    end 
 
     ## Create a sample writer for a port ##
     sampleWriter = rockControl.motion_command.writer
-
     sample = sampleWriter.new_sample
 
     ## glue the widget to the task writer
@@ -47,8 +42,5 @@ Orocos.run 'rock_tutorial::RockTutorialControl' => 'rock_tutorial_control' do
     
     ## Start the tasks ##
     rockControl.start
-    
-    ## Write motion command sample ##
-
     Vizkit.exec
 end
