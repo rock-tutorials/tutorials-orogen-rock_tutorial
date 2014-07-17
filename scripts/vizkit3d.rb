ad9652bc06f12c97b766039c925b6f3d39e6182f
Jakob Schwendner <jakob.schwendner@dfki.de>
2014-07-17 17:41:01 +0200

    fixed vizkit3d script
    I have no idea in which tutorial it belongs though.

--- scripts/vizkit3d.rb
+++ scripts/vizkit3d.rb
@@ -2,13 +2,13 @@ require 'vizkit'
 Orocos.initialize
 
 ## create a widget for 3d display
-view3d = Vizkit.default_loader.create_widget('vizkit::Vizkit3DWidget')
+view3d = Vizkit.vizkit3d_widget
 
 #show it
 view3d.show()
 
 ## load and add the 3d plugin for the rock
-rbs_plugin = view3d.createPlugin('RigidBodyStateVisualization')
+rbs_plugin = Vizkit.default_loader.RigidBodyStateVisualization
 rbs = Types::Base::Samples::RigidBodyState.new
 
 cnt = 0
@@ -21,7 +21,7 @@ timer.connect(SIGNAL("timeout()")) do
     rbs.position = Eigen::Vector3.new(0, 5.0 / 50.0 * cnt, 0)
     rbs.orientation = Eigen::Quaternion.new(0, 0, 0, 1)
 
-#    rbs_plugin.updateData(rbs)
+    rbs_plugin.updateData(rbs)
 end
 
 timer.start(100)
